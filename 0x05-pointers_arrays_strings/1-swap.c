5f9cfeb8e4c22782fc6f9be019a774f24a135e51
abdelhadi <abdelben11@outlook.fr>
2023-09-19 18:43:34 -0700

    a

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,20 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+* swap_int - swaps the value of int a and int b
+* Owned by Abdeljalil Ouafi
+* @a: first int
+* @b: second int
+* Return 0
+*/
+void swap_int(int *a, int *b)
 {
-    int a;
-    int b;
+		int s;
 
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
+		s = *a;
+		*a = *b;
+		*b = s;
 }
