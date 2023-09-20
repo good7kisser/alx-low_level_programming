a4c92e673c8a6ae57d8fe0e3531cb66f316d404c
abdelhadi <abdelben11@outlook.fr>
2023-09-19 18:15:38 -0700

    a

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,13 @@
 #include "main.h"
+
 /**
-*  reset_to_98 - takes the pointer and updates the value
-*  @n: int to check
-*  Owned By Abdelhadi Bensaber
-*  Return: 0 is success
-*/
+ * reset_to_98 - takes the pointer to an int parameter &nd update the value it points to 98
+ *
+ * @n: int parameter
+ *
+ * Return: Nothing
+ * 
+ */
 
 void reset_to_98(int *n)
 {
