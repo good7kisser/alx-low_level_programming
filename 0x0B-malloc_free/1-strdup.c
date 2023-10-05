0d29a88556a828b059e8037ae778713923a4de22
abdelhadi <abdelben11@outlook.fr>
2023-10-04 17:42:00 -0700

    a

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,36 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
 /**
- * *_strdup - copy of the string
- * @str: the string input
- * Return: NULL or pointer
- * owned by: SmoxHakim
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Writen by Abdelhadi Bensaber
+ * Return: 0
  */
-
 char *_strdup(char *str)
 {
-	size_t length = 0;
-	size_t i = 0;
-	char *duplicate;
+	char *aaa;
+	int i, r = 0;
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
-
-	while (str[length] != '\0')
-	{
-		length++;
-	}
-
-	duplicate = (char *)malloc((length + 1) * sizeof(char));
-
-	if (duplicate == NULL)
-	{
-		return (NULL);
-	}
-
+	i = 0;
 	while (str[i] != '\0')
-	{
+		i++;
+
+	aaa = malloc(sizeof(char) * (i + 1));
+
+	if (aaa == NULL)
+		return (NULL);
+
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
+
+	return (aaa);
+}
