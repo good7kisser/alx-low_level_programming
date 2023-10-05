b5d4a1a3be309b1ff8f4a136a9556ea7700ea1a3
abdelhadi <abdelben11@outlook.fr>
2023-10-04 18:35:26 -0700

    a

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,23 +4,23 @@
 /**
  * count_word - helper function to count the number of words in a string
  * @s: string to evaluate
- *
+ * Made by Abdelhadi Bensaber
  * Return: number of words
  */
 int count_word(char *s)
 {
-	int count, c, w;
+	int flag, c, w;
 
-	count = 0;
+	flag = 0;
 	w = 0;
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
-			count = 0;
-		else if (count == 0)
+			flag = 0;
+		else if (flag == 0)
 		{
-			count = 1;
+			flag = 1;
 			w++;
 		}
 	}
@@ -28,15 +28,3 @@ int count_word(char *s)
 	return (w);
 }
 /**
- * **strtow - splits a string into words
- * @str: string to split
- *
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
- */
-char **strtow(char *str)
-{
-	char **matrix, *tmp;
-	int i, k = 0, len = 0, words, c = 0, start, end;
-
-	while (*(str + len))
