525eb9cd495374f94495b0bc8d838ea574ee1266
abdelhadi <abdelben11@outlook.fr>
2023-10-04 18:30:48 -0700

    a

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -27,12 +27,3 @@ int _strlen(char *s)
  * @n: the number of bytes
  * Return: dest
  */
-
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i;
-
-	i = 0;
-	while (src[i] && i < n)
-	{
-		dest[i] = src[i];
