4f2445262e55cfae1adc0c10094094116572fad7
good7kisser <abdelben11@outlook.fr>
2023-10-24 16:55:08 -0700

    a

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,7 +4,7 @@
 /**
  * free_listint2 - frees a listint_t list and sets head to NULL
  * @head: a pointer to the head of the list
- * created by: goof7kisser
+ * created by: good7kisser
  */
 
 void free_listint2(listint_t **head)
